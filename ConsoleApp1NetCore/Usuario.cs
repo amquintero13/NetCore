8b12927113b6112f7aaddf30b686cab18aa40dc3
Andres Quintero <a.quintero@binarylatam.com>
2024-08-31 22:43:00 -0500

    Finalizar Implementación
    - Agregar script de BD
    - Agregar clase para el manejo de los usuarios
    - implementar el uso de los PA solicitados para crear y obtener los usuarios según la necesidad
    - Escribir los datos necesarios en el LOG

---
+++ ConsoleApp1NetCore/Usuario.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1NetCore
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Hobbies { get; set; }
+
+        // Constructor
+        public Usuario(string nombre, string apellido, int edad, string correo, string hobbies)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Correo = correo;
+            Hobbies = hobbies;
+        }
+
+        public string ObtenerNombreCompleto()
+        {
+            return Nombre + " " + Apellido;
+        }
+    }
+}
